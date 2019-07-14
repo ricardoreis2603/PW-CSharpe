a132f833dc522bf3269ca865cbfa1bfc6635c9d3
Ricardo Da Silva Reis <66046@PROWAY.TREINA>
2019-07-14 15:00:44 -0300

    Projeto aula1

---
+++ aula1/Pessoa.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Anotações: .vb; .cs para C#; .java para Java; é possível 2 linguagens para o mesmo programa, pois o .NET assim permite, mas não é recomendável
+
+/*
+ * 
+ * para comentário longo
+ * 
+ */
+
+//Atalho 'Ctrl k c' cria comentário para o conteúdo selecionado; 'Ctrl k u' volta tudo
+//Atalho 'Ctrl l' apaga uma linha
+//Atalho 'Windows' e abre o explorador de arquivos
+
+namespace aula1//cria um grupo classes, ou seja, o namespace é um grupamento de classes; forma-se o namespace, assim, organizam-se distintas classes
+{
+    class Pessoa
+    {
+        public string Nome { get; set; }//get pega o método; set atribui o método; a propriedade get set no .NET é uma forma para armazenar o conteúdo dentro da 
+        public string NumeroTelefone { get; set;}
+        public string Arquivo { get; set; }
+
+        public Pessoa()//atalho para o construtor 'ctor tab tab'  
+        {
+            Arquivo = @"\\svprwy001\Alunos\AlunosMelhorLinguagem\ListaDeContatos.txt";//@ ignora os caracteres especiais no string; o caminho onde o arquivo é gerado é C:\Users\65471\source\repos\aula1.1\aula1\bin\Debug; acessar via solução - botão direito - abrir pasta no gerenciador de arquivos
+        }
+
+        public string DadosPessoa()
+        {
+            string pessoaDados = Nome + "-" + NumeroTelefone;
+
+            return pessoaDados;
+        }
+
+        public void Gravar(Pessoa p)//Gravar é método, dentro dele há a inteligência/lógica
+        {
+            StreamWriter stream;//ctrl . e selecionar System.IO; StreamWriter para escrever na memória
+            stream = File.AppendText(Arquivo);
+            stream.WriteLine( p.DadosPessoa() ); 
+            stream.Close(); 
+        }
+
+        public List<Pessoa> Ler()
+        {
+            List<Pessoa> pessoas = new List<Pessoa>();
+
+            StreamReader stream;//StreamReader para ler a informação na memória
+            stream = File.OpenText(Arquivo);
+            while (stream.EndOfStream != true)//whi mais tab tab é o atalho; '!= true' corresponde a '== false'
+            {
+                string linha = stream.ReadLine();
+                string[] pessoaLinha = linha.Split('-');//o Split serve para quebrar uma string de acordo com o caracter
+                Pessoa pessoa = new Pessoa(); 
+                pessoa.Nome = pessoaLinha[0];
+                pessoa.NumeroTelefone = pessoaLinha[1];
+                pessoas.Add(pessoa);
+            }
+            stream.Close();
+            return pessoas; 
+
+            //List<Pessoa> pessoas = new List<Pessoa>();//List faz com que se retorne vários dados (pessoas); é uma verdadeira lista; (); chama o construtor da classe
+
+            //Pessoa pessoa1 = new Pessoa();
+            //pessoa1.Nome = "João";
+            //pessoa1.NumeroTelefone = "1111-1111";
+
+            //Pessoa pessoa2 = new Pessoa();
+            //pessoa2.Nome = "Maria";
+            //pessoa2.NumeroTelefone = "2222-2222";
+
+            //pessoas.Add(pessoa1);
+            //pessoas.Add(pessoa2);
+            return pessoas;
+
+        }
+    }
+}
---
+++ aula1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;// está mais em branco pois está sendo utilizada
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// > Aula 07/07/2019 - anotações.
+// - C# Fundamentos: solução é um contêiner que abarca vários projetos; C# é linguagem orientada a objetos.
+
+namespace aula1
+{
+    class Program//termos reservados para a linguagem; pode usar o underline para diferenciar
+    {
+        static void Main(string[] args)//classe padrão; sempre irá mostrar estes parâmetros; dentro da classe há os métodos; String, com letra maiúscula, tem mais recursos que o string
+                                       //método Main retorna o static void   
+        {
+            Console.WriteLine("LISTA DE ALUNOS");
+            Console.WriteLine(" ");
+
+            Pessoa pessoa = new Pessoa();//atalho 'Ctrl .' dá sugestões quando estiver sublinhado em vermelho; projetos precisam estar no mesmo nível de pastas
+
+            Console.Write("Digite o nome: ");
+            pessoa.Nome = Console.ReadLine();
+
+            Console.Write("Digite o telefone: ");
+            pessoa.NumeroTelefone = Console.ReadLine();//overflow ocorre quando estoura o número que se tentou armazenar na variável; solução, muda-se de int para string
+
+            string pessoaDados = pessoa.Nome + " - " + pessoa.NumeroTelefone;
+            Console.WriteLine(pessoaDados);
+
+            string dados = pessoa.DadosPessoa();//dá para chamar o método .DadosPessoa, o Visual Studio indica-o automaticamente
+            Console.WriteLine(dados);
+
+            pessoa.Gravar(pessoa); 
+
+
+            //List<Pessoa> pessoas = pessoa.Ler();
+
+            //Console.WriteLine(pessoas.Count);//contador
+            //Console.WriteLine("Contador.");
+
+            //Console.WriteLine ( pessoas.Last() );//exibe o último objeto da lista
+            //Console.WriteLine("Último da lista.");
+
+            //Console.WriteLine ( pessoas.Last().Nome );//exibe o último objeto da lista com o nome
+            //Console.WriteLine(" ");
+
+            //foreach (Pessoa p in pessoas)//Pessoa pode ser substituído por var; para formar um laço de repetição
+            //{
+            //    Console.WriteLine( p.DadosPessoa() );
+            //}
+            //Console.WriteLine(" ");
+
+            //Pessoa p1 = pessoas.Last();//Last pode ser substituído por First
+            //Console.WriteLine( p1.DadosPessoa() );
+            //Console.WriteLine(" ");
+           
+
+            Console.ReadLine();
+
+
+            //primeiro código
+
+            string frase;//string é conjunto de letras; declaração da variável; para armazenar este tipo de dado; int é para números inteiros
+
+            Console.WriteLine("Olá mundo.");//escreve uma linha 
+
+            Console.Write("Digite uma frase: ");
+            frase = Console.ReadLine(); //lê e mostra a frase, isto é, o conteúdo armazenado dentro da variável frase
+
+            Console.Write("A frase escrita foi: ");//no Write não se pula linha; no WriteLine pula a linha
+            Console.WriteLine(frase);//atalho 'cw tab tab' cola o código: Console.WriteLine();  
+
+            Console.ReadLine();
+        }
+    }
+}
